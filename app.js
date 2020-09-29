8b2f46986d67013d33a2b1008a1cdca94e88cfc4
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-29 18:02:01 -0500

    2.3 Crear funciones y usar npm init
    Node JS usa la sintaxis de javascript, tambien usamos el comando npm init para crear un nuevo proyecto npm

---
+++ app.js
@@ -0,0 +1,10 @@
+var valor_1 = "Hola";
+var valor_2 = "Mundo";
+var saludar = valor_1 + " " + valor_2;
+console.log(saludar);
+
+function sumar(a, b){
+    return a + b;
+}
+
+console.log(sumar(6,8));
