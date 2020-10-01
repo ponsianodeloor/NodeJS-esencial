dc0fd5b120f4a4d7c7eea236f2d09734930e3ecd
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-01 10:29:49 -0500

    4.3.- Operar un Servidor Node, Desplegar contenido en el servidor
    Desplegar contenido en el servidor

--- 004 Operar un Servidor Node/4.2 Crear un servidor basico/app.js
+++ 004 Operar un Servidor Node/4.2 Crear un servidor basico/app.js
@@ -1,7 +1,13 @@
 var http = require("http");
 
-var servidor = http.createServer( function(){
+/**
+ * peticion obtiene informacion del url, cliente (Mozilla, Chrome), hora, ip
+ * respuesta
+ */
+var servidor = http.createServer( function(peticion, respuesta){
     console.log("peticion web");
+    respuesta.writeHead(200, {'Content-type': 'text/html'});
+    respuesta.write("Respuesta para la direccion: " + peticion.url);
     
 });
 
