97b32030bb921c3b32daacf04b8782855a077ea0
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-01 23:46:05 -0500

    4.7.- Operar un servidor Node, Enrutado con proyectos creados por Express Generator

--- 004 Operar un Servidor Node/4.4 Instalar express y express generator/expressbase/app.js
+++ 004 Operar un Servidor Node/4.4 Instalar express y express generator/expressbase/app.js
@@ -1,26 +1,43 @@
-var express = require("express");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var pruebaRouter = require('./routes/prueba');
+
 var app = express();
 
-app.all('/', function(peticion, respuesta){
-  respuesta.send("Hola");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/prueba', pruebaRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/about', function(peticion, respuesta){
-  respuesta.send('Hola desde acerca')
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.post('/about', function(peticion, respuesta){
-  respuesta.send('Has enviado datos de un formulario')
-});
-
-app.delete('/about', function(peticion, respuesta){
-  respuesta.send('Hola desde acerca')
-});
-
-app.put('/about', function(peticion, respuesta){
-  respuesta.send('Hola desde acerca')
-});
-
-var server = app.listen(3000, function(){
-
-});
+module.exports = app;
