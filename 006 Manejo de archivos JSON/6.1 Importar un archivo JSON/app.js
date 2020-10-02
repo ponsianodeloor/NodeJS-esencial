d295d22aa8b9070d8eb3de5881da963c79e606b9
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-02 18:25:36 -0500

    6.1.1.- Manejo de archivos, Importar un archivo de forma asíncrona

--- 006 Manejo de archivos JSON/6.1 Importar un archivo JSON/app.js
+++ 006 Manejo de archivos JSON/6.1 Importar un archivo JSON/app.js
@@ -1,2 +1,11 @@
+//async
+var fs = require('fs');
+fs.readFile('./archivos/libros.json', (error, datos)=>{
+    console.log(JSON.parse(datos).autor);
+    
+});
+
 var rutaJSON = require('./archivos/libros.json');
 console.log(rutaJSON.autor);
+
+
