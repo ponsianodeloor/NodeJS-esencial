add5db2f21a0a2fc1a01a25cbc8146123bb9e59d
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-01 23:34:41 -0500

    4.6.- Operar un servidor Node, Enrutado según Verbos de HTTP

--- 004 Operar un Servidor Node/4.4 Instalar express y express generator/expressbase/app.js
+++ 004 Operar un Servidor Node/4.4 Instalar express y express generator/expressbase/app.js
@@ -5,6 +5,22 @@ app.all('/', function(peticion, respuesta){
   respuesta.send("Hola");
 });
 
+app.get('/about', function(peticion, respuesta){
+  respuesta.send('Hola desde acerca')
+});
+
+app.post('/about', function(peticion, respuesta){
+  respuesta.send('Has enviado datos de un formulario')
+});
+
+app.delete('/about', function(peticion, respuesta){
+  respuesta.send('Hola desde acerca')
+});
+
+app.put('/about', function(peticion, respuesta){
+  respuesta.send('Hola desde acerca')
+});
+
 var server = app.listen(3000, function(){
 
 });
