e062dbe5e51c78587e9cacc2cfad621ca0311a98
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-02 00:14:35 -0500

    4.10.- Operar un servidor Node, Desplegar variables en PUG

--- 004 Operar un Servidor Node/4.4 Instalar express y express generator/expressbase/routes/dinamico.js
+++ 004 Operar un Servidor Node/4.4 Instalar express y express generator/expressbase/routes/dinamico.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(peticion, respuesta, next) {
-  respuesta.send('Informacion dinamica');
+  //respuesta.send('Informacion dinamica');
+  respuesta.render('dinamico', { title: 'Dinamico' });
 });
 
 router.get('/:datoURL/:otroParametro', function(peticion, respuesta, next) {
-  respuesta.send('Informacion dinamica: ' + peticion.params.datoURL + peticion.params.otroParametro);
+  //respuesta.send('Informacion dinamica: ' + peticion.params.datoURL + peticion.params.otroParametro);
+  respuesta.render('dinamico', { 
+    title: 'Dinamico',
+    datos:{
+      parametroURL1: peticion.params.datoURL,
+      parametroURL2: peticion.params.otroParametro
+    }
+   });
 });
 
 module.exports = router;
