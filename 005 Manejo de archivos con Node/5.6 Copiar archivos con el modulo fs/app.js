acdd8b6e04825e1fc1a971cc27aded77dbd60c47
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-02 12:10:21 -0500

    5.6.- Manejo de archivos, Copiar archivos con el modulo fs

---
+++ 005 Manejo de archivos con Node/5.6 Copiar archivos con el modulo fs/app.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.copyFile('./archivos/archivo_original.txt', './archivos/archivo_copia.txt', (error)=>{
+    if (error) {
+        console.log('Ha existido un error ', error);
+    } else {
+        console.log('archivo copiado correctamente');
+    }
+});
